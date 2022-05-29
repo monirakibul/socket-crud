c6cddac2d21256769cce866e8719df577a81ce20
monirakibul <monirakibul@gmail.com>
2022-05-29 22:40:48 +0600

    Comoleted

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const http = require("http")
+const socketIO = require("socket.io");
+
+app.use(cors());
+
+
+const server = http.createServer(app)
+    ;
+
+
+const io = socketIO(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "DELETE", "PUT"]
+    },
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqdj2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('simpleCrud').collection('items')
+        io.on("connection", (socket) => {
+            socket.on("get_items", async () => {
+                const items = await itemsCollection.find().toArray();
+                io.emit("get_items", items);
+            });
+
+            socket.on("edit_item", async (data) => {
+                const id = data.id;
+                const item = {
+                    title: data.title,
+                    description: data.description
+                }
+                const filter = { _id: ObjectId(id) };
+                const option = { upsert: true };
+                const updateDoc = {
+                    $set: item,
+                };
+                await itemsCollection.updateOne(filter, updateDoc, option);
+                const items = await itemsCollection.find().toArray();
+                io.emit("get_items", items);
+            });
+
+            socket.on("add_item", async (data) => {
+                const item = {
+                    title: data.title,
+                    description: data.description
+                }
+                await itemsCollection.insertOne(item);
+                const items = await itemsCollection.find().toArray();
+                io.emit("get_items", items);
+            });
+
+            socket.on("delete_item", async (data) => {
+                const id = data.id;
+                const filter = { _id: ObjectId(id) };
+                await itemsCollection.deleteOne(filter);
+                const items = await itemsCollection.find().toArray();
+                io.emit("get_items", items);
+            });
+        })
+    }
+    finally { }
+}
+run().catch(console.dir)
+
+server.listen(5000, () => {
+    console.log("Server is running");
+})
